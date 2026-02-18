8e196bd02a4da48158db1e23e1b9a3d5250878ba
sam170203 <mudgalsaksham@gmail.com>
2026-02-18 18:04:00 +0000

    Add CORS support for frontend connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const hotelRoutes = require('./routes/hotel.routes');
 const bookingRoutes = require('./routes/booking.routes');
 const reviewRoutes = require('./routes/review.routes');
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://sam170203.github.io'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
@@ -16,8 +24,3 @@ app.use('/api/bookings', bookingRoutes);
 app.use('/api/reviews', reviewRoutes);
 
 module.exports = app;
-
-
-
-
-
